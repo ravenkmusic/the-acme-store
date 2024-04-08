4fe66f8cfdab7c48c3c81caf6f5cdbd40946310d
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-08 16:14:15 -0400

    added dummy data

--- server/index.js
+++ server/index.js
@@ -29,12 +29,29 @@ const init = async ()=> {
         createProducts({name: 'Swiffer'}),
         createProducts({name: 'Dyson'}),
         createProducts({name: 'Tide'}),
-        createProducts({name: 'Palmolive'}),
+        createProducts({name: 'Palmolive'})
     ]);
 
     console.log(await fetchUsers(), 'Users fetched.');
     console.log(await fetchProducts(), 'Products fetched.');
 
+    const favorites = await Promise.all([
+        createFavorite({user_id: Grace.id, products_id: Palmolive.id}),
+        createFavorite({user_id: Krystin.id, products_id: Dyson.id }),
+        createFavorite({user_id: Arnold.id, products_id: Swiffer.id}),
+        createFavorite({user_id: Lauren.id, products_id: Swiffer}),
+        createFavorite({user_id: Grace.id, products_id: Tide.id}),
+        createFavorite({user_id: Arnold.id, products_id: Dyson.id})
+    ]);
+
+    console.log(await fetchFavorites(Lauren.id));
+    await destroyFavorite({user_id: Lauren.id, products_id: favorites[0].id});
+    console.log(await fetchFavorites(Lauren.id));
+
+    console.log(`curl -X POST localhost:3000/api/users/${Grace.id}/favorites -d '{"products_id": "${Tide.id}"}' -H 'Content-Type:application/json'`);
+    console.log(`curl -X DELETE localhost:3000/api/users/${Arnold.id}/favorites/${favorites[0].id}`);
+
+    console.log('Data seeded.');
     const port = process.env.PORT || 3000;
     app.listen(port, ()=> console.log(`Now listening on port ${port}.`));
 }
