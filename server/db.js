56c88c7f70ab2089a724861aff7f94d4534b7cdd
Raven Hendricks <ravenkbeats@gmail.com>
2024-04-08 15:00:04 -0400

    created all methods to modify database

--- server/db.js
+++ server/db.js
@@ -11,38 +11,81 @@ const createTables = async () => {
 
         CREATE TABLE user(
             id UUID PRIMARY KEY,
-            username VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL
+            username VARCHAR(20) NOT NULL UNIQUE,
+            password VARCHAR(20) NOT NULL
         );
 
         CREATE TABLE products(
             id UUID PRIMARY KEY,
-            name VARCHAR(255)
+            name VARCHAR(255) NOT NULL UNIQUE
         );
 
         CREATE TABLE favorite(
             id UUID PRIMARY KEY,
             product_id UUID REFERENCES products(id) NOT NULL,
             user_id UUID REFERENCES user(id) NOT NULL,
-            CONSTRAINT user_id product_id UNIQUE
+            CONSTRAINT unique_user_id_product_id UNIQUE (user_id, product_id)
         );
     `;
     await client.query(SQL);
 };
 
-const createProduct = async () => {};
+const createProduct = async (name) => {
+    const SQL = `
+        INSERT INTO product(id, name) VALUES($1, $2) RETURNING *;
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+    return response.rows[0];
+};
 
-const createUser = async () => {};
+const createUser = async ( {username, password}) => {
+    const SQL = `
+        INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *;
+    `;
+    const response = await client.query(SQL, [uuid.v4(), username, bcrypt.hash(password, 10)]);
+    return response.rows[0];
+};
 
-const fetchUsers = async () => {};
+const fetchUsers = async () => {
+    const SQL = `
+        SELECT id, username FROM user;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+    const SQL = `
+        SELECT * FROM product;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
 
-const fetchFavorites = async () => {};
+const fetchFavorites = async (user_id) => {
+    const SQL = `
+        SELECT * FROM favorite
+        WHERE user_id = $1
+    `;
+    const response = await client.query(SQL, [user_id]);
+    return response.rows;
+};
 
-const createFavorite = async () => {};
+const createFavorite = async ({product_id, user_id}) => {
+    const SQL = `
+        INSERT INTO favorite(id, product_id, user_id) VALUES ($1, $2, $3);
+    `;
+    const response = await client.query(SQL, [uuid.v4(), product_id, user_id]);
+    return response.rows[0];
+};
 
-const destroyFavorite = async () => {};
+const destroyFavorite = async ({user_id, id}) => {
+    const SQL = `
+        DELETE FROM favorite
+        WHERE user_id = $1 AND id = $2
+    `;
+    await client.query(SQL);
+};
 
 
 module.exports = {
